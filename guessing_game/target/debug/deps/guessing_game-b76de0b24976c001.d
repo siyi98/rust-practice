7bddf0710dcfaa1fa884a6dee8690e57b68a8c89
siyi98 <zhusijia1998@gmail.com>
2022-07-17 21:18:25 +0800

    feat: delete target

--- guessing_game/target/debug/deps/guessing_game-b76de0b24976c001.d
+++
@@ -1,5 +0,0 @@
-/Users/siyi/Desktop/open-source/rust-practice/guessing_game/target/debug/deps/guessing_game-b76de0b24976c001: src/main.rs
-
-/Users/siyi/Desktop/open-source/rust-practice/guessing_game/target/debug/deps/guessing_game-b76de0b24976c001.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/siyi/Desktop/open-source/rust-practice/guessing_game/target/debug/guessing_game: /Users/siyi/Desktop/open-source/rust-practice/guessing_game/src/main.rs
