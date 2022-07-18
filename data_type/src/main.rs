479e507a8817c102c0dbf28f22845608726326bd
siyi98 <zhusijia1998@gmail.com>
2022-07-18 14:43:23 +0800

    feat: update

---
+++ data_type/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _floored = 2 / 3; // Results in 0
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    let _t = true;
+
+    let _f: bool = false; // with explicit type annotation
+}
